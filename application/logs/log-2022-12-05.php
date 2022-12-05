15d0872c353412eac0dba87c7fa1d80f44620767
fcmirfan122 <fcmirfan122@gmail.com>
2022-12-05 16:52:15 +0700

    add app

---
+++ application/logs/log-2022-12-05.php
@@ -0,0 +1,50 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-12-05 09:43:24 --> Config Class Initialized
+INFO - 2022-12-05 09:43:24 --> Hooks Class Initialized
+DEBUG - 2022-12-05 09:43:24 --> UTF-8 Support Enabled
+INFO - 2022-12-05 09:43:24 --> Utf8 Class Initialized
+INFO - 2022-12-05 09:43:24 --> URI Class Initialized
+DEBUG - 2022-12-05 09:43:24 --> No URI present. Default controller set.
+INFO - 2022-12-05 09:43:24 --> Router Class Initialized
+INFO - 2022-12-05 09:43:24 --> Output Class Initialized
+INFO - 2022-12-05 09:43:24 --> Security Class Initialized
+DEBUG - 2022-12-05 09:43:24 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-05 09:43:24 --> Input Class Initialized
+INFO - 2022-12-05 09:43:24 --> Language Class Initialized
+INFO - 2022-12-05 09:43:24 --> Loader Class Initialized
+INFO - 2022-12-05 09:43:24 --> Helper loaded: url_helper
+INFO - 2022-12-05 09:43:24 --> Controller Class Initialized
+INFO - 2022-12-05 09:43:24 --> File loaded: E:\project\sekolah\digiup\digiupci-main\digiupci-main\application\views\welcome_message.php
+INFO - 2022-12-05 09:43:24 --> Final output sent to browser
+DEBUG - 2022-12-05 09:43:24 --> Total execution time: 0.2583
+INFO - 2022-12-05 09:43:40 --> Config Class Initialized
+INFO - 2022-12-05 09:43:40 --> Hooks Class Initialized
+DEBUG - 2022-12-05 09:43:40 --> UTF-8 Support Enabled
+INFO - 2022-12-05 09:43:40 --> Utf8 Class Initialized
+INFO - 2022-12-05 09:43:40 --> URI Class Initialized
+INFO - 2022-12-05 09:43:40 --> Router Class Initialized
+INFO - 2022-12-05 09:43:40 --> Output Class Initialized
+INFO - 2022-12-05 09:43:40 --> Security Class Initialized
+DEBUG - 2022-12-05 09:43:40 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-05 09:43:40 --> Input Class Initialized
+INFO - 2022-12-05 09:43:40 --> Language Class Initialized
+ERROR - 2022-12-05 09:43:40 --> 404 Page Not Found: Ekatalog/index
+INFO - 2022-12-05 09:43:52 --> Config Class Initialized
+INFO - 2022-12-05 09:43:52 --> Hooks Class Initialized
+DEBUG - 2022-12-05 09:43:52 --> UTF-8 Support Enabled
+INFO - 2022-12-05 09:43:52 --> Utf8 Class Initialized
+INFO - 2022-12-05 09:43:52 --> URI Class Initialized
+DEBUG - 2022-12-05 09:43:52 --> No URI present. Default controller set.
+INFO - 2022-12-05 09:43:52 --> Router Class Initialized
+INFO - 2022-12-05 09:43:52 --> Output Class Initialized
+INFO - 2022-12-05 09:43:52 --> Security Class Initialized
+DEBUG - 2022-12-05 09:43:52 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-05 09:43:52 --> Input Class Initialized
+INFO - 2022-12-05 09:43:52 --> Language Class Initialized
+INFO - 2022-12-05 09:43:52 --> Loader Class Initialized
+INFO - 2022-12-05 09:43:52 --> Helper loaded: url_helper
+INFO - 2022-12-05 09:43:52 --> Controller Class Initialized
+INFO - 2022-12-05 09:43:52 --> File loaded: E:\project\sekolah\digiup\digiupci-main\digiupci-main\application\views\welcome_message.php
+INFO - 2022-12-05 09:43:52 --> Final output sent to browser
+DEBUG - 2022-12-05 09:43:52 --> Total execution time: 0.0131
